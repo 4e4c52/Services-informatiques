4e09d6be5ad05e3d3832cb13fb8e879e4d34e4cb
Nathan Le Ray <oyokoon@syrinxoon.net>
2011-06-26 14:49:24 +0200

    Finished tuts

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 # activate :code_ray
 
 # Automatic sitemaps (gem install middleman-slickmap)
-#require "middleman-slickmap"
-#activate :slickmap
+require "middleman-slickmap"
+activate :slickmap
 
 # Automatic image dimension calculations
 # activate :automatic_image_sizes
@@ -27,13 +27,17 @@ helpers do
       false
     end
   end
-  def gallery(image)
-   content_tag(:p, link_to(image_tag(image, :alt => "#"), "images/" + image, :rel => :fancybox), :class => :gallery)
+  def gallery(image, tuts = false)
+    if tuts
+      content_tag(:p, link_to(image_tag(image, :alt => "#"), "../images/" + image, :rel => :fancybox), :class => :gallery)
+    else
+      content_tag(:p, link_to(image_tag(image, :alt => "#"), "images/" + image, :rel => :fancybox), :class => :gallery)
+    end
   end
 end
 
 # Change the CSS directory
-# set :css_dir, "./stylesheets/"
+# set :css_dir, "alternative_css_directory"
 
 # Change the JS directory
 # set :js_dir, "alternative_js_directory"
@@ -44,10 +48,10 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
   activate :cache_buster
@@ -56,8 +60,8 @@ configure :build do
   activate :relative_assets
   
   # Compress PNGs after build (gem install middleman-smusher)
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Generate ugly/obfuscated HTML from Haml
   # activate :ugly_haml
